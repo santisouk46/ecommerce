1e1ab7694ff23e8f4041cfafc4060cf0f15337ce
santisouk46 <k.santisouk@sku.edu.la>
2025-08-10 09:46:11 +0700

    migrate mookshop database

--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,13 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+use App\Http\Controllers\HomeController;
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('/', [HomeController::class, 'index']);
 
 Route::middleware([
     'auth:sanctum',
@@ -14,4 +18,9 @@ Route::middleware([
     Route::get('/dashboard', function () {
         return view('dashboard');
     })->name('dashboard');
+});
+
+
+Route::get('/sabaidee/{name}', function ($name) {
+    echo "Sabaidee Mr.".$name;
 });
